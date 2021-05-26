fb32f60aadf0eb07140dca058da36e62ca7346b2
Bar Bar Jinks <54543475+PalliateAi@users.noreply.github.com>
2021-05-26 08:12:55 +0100

    reupload

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import { Button, Alert, Breadcrumb, Card, Form } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Card className="mb-3" style={{ color: "#000"}}>
-          <Card.Img src="https://picsum.photos/200/100"/>
-          <Card.Body>
-            <Card.Title>
-              Card Example
-            </Card.Title>
-            <Card.Text>
-              This is the example
-            </Card.Text>
-            <Button variant="primary">Another Button</Button>
-          </Card.Body>
-        </Card>
-        <Breadcrumb>
-          <Breadcrumb.Item>Test</Breadcrumb.Item>
-          <Breadcrumb.Item>Test</Breadcrumb.Item>
-          <Breadcrumb.Item>Test</Breadcrumb.Item>
-        </Breadcrumb>
-        <Alert variant="primary">Alert Alert Dudeee</Alert>
-        <Button>Test Button</Button>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/products" component={Products} />
+          <Route path="/sign-up" component={SignUp} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
